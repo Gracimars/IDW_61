943598ed3570c67680ee7fbdfefe82217804563c
Cristopher Yang <dg.cris.yang@gmail.com>
2025-10-17 20:20:10 -0300

    preservar ID en edicion && editar y crear medico

--- scripts/medicos/crudMedicos.js
+++ scripts/medicos/crudMedicos.js
@@ -6,14 +6,28 @@ const getQueryParams = () => {
     };
 }
 
+const getMedicosDatabase = () => {
+    const medicos = localStorage.getItem("dbMedicos") || JSON.stringify([]);
+    return JSON.parse(medicos);
+}
+
+
+
+
 const handleSubmit = (event) => {
     event.preventDefault();
+    const medicosDB = getMedicosDatabase();
+    // console.log(medicosDB);
     const form = event.target;
     const formData = Object.fromEntries(new FormData(form));
     const { action, id } = getQueryParams();
-    console.log(formData, action, id)
+    // console.log(formData, action, id)
+
+
+    // console.log((Math.max(...medicosDB.map(medico => medico.id)) + 1))
 
     const doctorEntry = {
+        id: action === "edit" && id ? parseInt(id) : (Math.max(...medicosDB.map(medico => medico.id)) + 1),
         nombre: formData.nombre,
         apellido: formData.apellido,
         matricula: parseInt(formData.matricula),
@@ -21,5 +35,16 @@ const handleSubmit = (event) => {
         obrasSociales: Object.keys(formData).filter(key => key.endsWith("OS")).map(key => formData[key])
     }
 
-    console.log(doctorEntry)
+    if (action === "edit" && id) {
+        const index = medicosDB.findIndex(medico => medico.id === parseInt(id));
+        if (index !== -1) {
+            medicosDB.splice(index, 1, doctorEntry)
+        } else {
+            medicosDB.push(doctorEntry)
+        }
+    } else {
+        medicosDB.push(doctorEntry)
+    }
+
+    localStorage.setItem("dbMedicos", JSON.stringify(medicosDB));
 }
