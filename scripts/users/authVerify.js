48c80a77060b6efded8f8b23a272913bca8ba1e5
Cristopher Yang <dg.cris.yang@gmail.com>
2025-10-23 16:34:00 -0300

    hotfix: relative path for GH Pages deployment in login script

--- scripts/users/authVerify.js
+++ scripts/users/authVerify.js
@@ -1,52 +1,54 @@
 const getUsersDatabase = () => {
-  const users = localStorage.getItem('dbUsers') || JSON.stringify([])
-  return JSON.parse(users)
-}
+  const users = localStorage.getItem("dbUsers") || JSON.stringify([]);
+  return JSON.parse(users);
+};
 
 const handleLogin = (event) => {
-  const usersDB = getUsersDatabase()
+  const usersDB = getUsersDatabase();
 
-  event.preventDefault()
-  const form = event.target
-  const formData = Object.fromEntries(new FormData(form))
+  event.preventDefault();
+  const form = event.target;
+  const formData = Object.fromEntries(new FormData(form));
 
   const loginUser = usersDB.find(
-    (user) => user.email === formData.email && user.password === formData.password
-  )
+    (user) =>
+      user.email === formData.email && user.password === formData.password
+  );
 
   if (!loginUser) {
-    sessionStorage.setItem('userRole', null)
-    alert('Credenciales inválidas. Por favor, intenta de nuevo.')
-    return
+    sessionStorage.setItem("userRole", null);
+    alert("Credenciales inválidas. Por favor, intenta de nuevo.");
+    return;
   }
 
-  sessionStorage.setItem('user', JSON.stringify(loginUser))
-  window.location.href = '/index.html'
-}
+  sessionStorage.setItem("user", JSON.stringify(loginUser));
+  window.location.href = "../../index.html";
+};
 
 const isUserLoggedIn = () => {
-  return sessionStorage.getItem('user') !== null
-}
+  return sessionStorage.getItem("user") !== null;
+};
 
-const isAdmin = () => JSON.parse(sessionStorage.getItem('user'))?.role === 'administrator'
+const isAdmin = () =>
+  JSON.parse(sessionStorage.getItem("user"))?.role === "administrator";
 
-const isProtectedRoute = () => window.location.pathname.includes('/admin/')
+const isProtectedRoute = () => window.location.pathname.includes("/admin/");
 
 const handleLogout = () => {
-  sessionStorage.removeItem('user')
-  window.location.href = '/index.html'
-}
+  sessionStorage.removeItem("user");
+  window.location.href = "../../pages/login.html";
+};
 
 if (isUserLoggedIn()) {
-  const authButtons = document.getElementById('auth-buttons-group')
-  authButtons?.classList?.add('d-none')
+  const authButtons = document.getElementById("auth-buttons-group");
+  authButtons?.classList?.add("d-none");
 }
 
 if (isAdmin()) {
-  const adminNavbar = document.getElementById('admin-access-routes')
-  adminNavbar.classList.remove('d-none')
+  const adminNavbar = document.getElementById("admin-access-routes");
+  adminNavbar.classList.remove("d-none");
 }
 
 if (isProtectedRoute() && !isAdmin()) {
-  window.location.href = '/pages/login.html'
+  window.location.href = "../../pages/login.html";
 }
